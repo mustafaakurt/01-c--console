e0cf00c6c5bcaded52fadeee2749d05109ee5196
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-02-20 00:13:16 +0300

    c#-encapsulation added

---
+++ 01-c#-console/07-c#-encapsulation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_c__console
+{
+    internal class _07_c__encapsulation
+    {
+        static void Main(String[] args)
+        {
+            Person person = new Person();
+            person.Name = "Mustafa";
+            person.Age = 25;
+            Console.WriteLine("Name: " + person.Name + " Age: " + person.Age);
+            Console.ReadKey();
+
+        }
+        // Encapsulation örneği 
+        public class Person
+        {
+            private string name;
+            private int age;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public int Age
+            {
+                get { return age; }
+                set { age = value; }
+            }
+        }
+    }
+}
